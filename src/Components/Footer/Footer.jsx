772ea348ed13d88da3177ed9954c405358dc5d8e
Toacin <toacinp@outlook.com>
2022-12-21 12:33:39 -0700

    footer updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,26 @@
 import './Footer.css'
-import {AiFillGithub, AiFillTwitterCircle, AiFillLinkedin} from 'react-icons/ai'
+import {AiFillGithub, AiFillTwitterCircle, AiFillLinkedin, AiOutlineMail, AiOutlinePhone} from 'react-icons/ai'
+import {TfiLocationPin} from 'react-icons/tfi';
 
 export default function Footer() {
     return (
         <div id="footer">
-            <a rel="noreferrer" href="https://www.github.com/Toacin" target="_blank"><AiFillGithub/></a>
-            <a rel="noreferrer" href="https://www.linkedin.com/in/toacin-patwary-985728256/" target="_blank"><AiFillLinkedin/></a>
-            <a rel="noreferrer" href="https://www.twitter.com/twaseeen" target="_blank"><AiFillTwitterCircle/></a>
+            <div id="footer-contact-wrapper">
+                {/* <h2 id='footer-contact-title'>Connect</h2>
+                <div className="vl"></div> */}
+                <div className="footer-contact-elements-wrapper">
+                    <p className='footer-contact-elements'><AiOutlineMail className='footer-contact-icon'/> toacinp@outlook.com</p>
+                    <div className="vl2"></div>
+                    <p className='footer-contact-elements'><AiOutlinePhone className='footer-contact-icon'/> +1 (602) 687-3713</p>
+                    <div className="vl2"></div>
+                    <p className='footer-contact-elements'><TfiLocationPin className='footer-contact-icon'/> Phoenix, AZ</p>
+                </div>
+            </div>
+            <div id='footer-nav'>
+                <a rel="noreferrer" href="https://www.github.com/Toacin" target="_blank"><AiFillGithub/></a>
+                <a rel="noreferrer" href="https://www.linkedin.com/in/toacin-patwary-985728256/" target="_blank"><AiFillLinkedin/></a>
+                <a rel="noreferrer" href="https://www.twitter.com/twaseeen" target="_blank"><AiFillTwitterCircle/></a>
+            </div>
         </div>
     )
 }
--- src/Components/Projects/project.jsx
+++ src/Components/Projects/project.jsx
@@ -1,12 +1,20 @@
 import './project.css'
 import { AiFillGithub } from 'react-icons/ai';
+import { ImLink } from 'react-icons/im'
 
 export default function Project({projectInfo}) {
     return (
         <a className="content-card col-11 col-lg-5 my-4" id={projectInfo.id} href={projectInfo.href} target="_blank">
             <div></div>
             <p>{projectInfo.projectInfo}</p>
-            <h4>{projectInfo.projectTitle} <AiFillGithub/></h4>
+            <h4 style={{display: "flex", alignItems: "center"}}>
+                {projectInfo.projectTitle}
+                <AiFillGithub onClick={(e)=>{
+                    e.stopPropagation();
+                    window.open("https://www.google.com", "_blank")
+                }} className='github-icon'/>
+                <ImLink/>
+            </h4>
         </a>
     )
 }
